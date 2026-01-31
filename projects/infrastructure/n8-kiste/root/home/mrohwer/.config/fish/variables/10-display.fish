dce0d7b41cfc65f3b4b5c2b03f07eabb9a327fce
Michael Rohwer <git@mr-bytez.de>
2026-01-31 01:16:09 +0100

    [Config] n8-kiste: HiDPI-Variablen deaktiviert
    - GDK_SCALE und QT_SCALE_FACTOR auskommentiert
    - Skalierung jetzt über KDE Anzeigeverwaltung
    - Sauberer systemweiter Ansatz statt Fish-Variablen

--- projects/infrastructure/n8-kiste/root/home/mrohwer/.config/fish/variables/10-display.fish
+++ projects/infrastructure/n8-kiste/root/home/mrohwer/.config/fish/variables/10-display.fish
@@ -2,6 +2,6 @@
 # n8-kiste Display-Variablen
 # ══════════════════════════════════════════════════════════════════════════════
 
-set -gx GDK_SCALE 2
-set -gx QT_SCALE_FACTOR 2
+#set -gx GDK_SCALE 2
+#set -gx QT_SCALE_FACTOR 2
 set -gx N8_HOST_TEST "n8-kiste-variable-works"
