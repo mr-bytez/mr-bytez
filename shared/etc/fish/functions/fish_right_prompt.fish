93b07c5048003678a329bff17d17dc2afd3e9218
Michael Rohwer <mrohwer@mr-bytez.de>
2026-02-03 23:24:20 +0100

    [Fix][Fish][Deployment] Fish nach /etc/fish verschoben + Right-Prompt Status-Fix
    Änderungen:
    - [Deployment] Fish-Config von /usr/local/share/fish nach /etc/fish verschoben
      Grund: Fish lädt /etc/fish automatisch, /usr/local/share/fish NICHT
    - [Fix] fish_right_prompt.fish: Status-Variable fix ($last_status statt $__mr_last_status)
    - [Config] shared/home/mrohwer/.config/fish/config.fish hinzugefügt
      Sources /etc/fish/conf.d/00-loader.fish
    
    Impact:
    - Systemweite Fish-Config wird automatisch geladen
    - Kein User-Symlink nötig
    - FHS-konform (/etc/fish für system-wide configs)
    
    Status: Getestet auf n8-kiste, funktioniert ✓

--- shared/etc/fish/functions/fish_right_prompt.fish
+++ shared/etc/fish/functions/fish_right_prompt.fish
@@ -1,23 +1,24 @@
 # ============================================
 # mr-bytez Fish Right Prompt
-# Version: 2.1.0
-# Deployment: shared/usr/local/share/fish/functions/
+# Version: 2.2.0
+# Deployment: shared/etc/fish/functions/
 # Benötigt: 00-theme.fish für Farben, Icons, Separatoren
 # ============================================
 
 function fish_right_prompt
     set -l sep $MR_SEP_RIGHT
+    set -l last_status $status  # Status direkt hier holen!
     
     # ============================================
     # CMD DURATION (wenn > 1 Sekunde)
     # ============================================
     
-    if test $CMD_DURATION
+    if test -n "$CMD_DURATION"  # -n statt nur test
         if test $CMD_DURATION -gt 1000
             set -l duration_sec (math "$CMD_DURATION / 1000")
             
             # Fehler? Rot + Symbol
-            if test $__mr_last_status -ne 0
+            if test $last_status -ne 0
                 set_color $MR_BG_ERROR
                 echo -n $sep
                 set_color --bold -b $MR_BG_ERROR $MR_TEXT_PRIMARY
@@ -39,7 +40,7 @@ function fish_right_prompt
     set_color $MR_BG_TIME
     echo -n $sep
     set_color --bold -b $MR_BG_TIME $MR_TEXT_PRIMARY
-    echo -n ' '(date '+%H:%M:%S')' '
+    echo -n " "(date "+%H:%M:%S")" "
     
     set_color normal
 end
---
+++ shared/home/mrohwer/.config/fish/config.fish
@@ -0,0 +1,2 @@
+set -g FISH_LOADER_DEBUG 1
+source /etc/fish/conf.d/00-loader.fish
